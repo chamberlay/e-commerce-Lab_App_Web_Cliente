2aac875da435be2407f6039689ff1bffed83d8bd
Chamberlay Vega <chamberlayvega@gmail.com>
2026-07-02 13:32:15 -0300

    reestructuracion y css

--- js/interfazUsuario.js
+++ js/interfazUsuario.js
@@ -1,36 +1,50 @@
 const contenedorProductos = document.getElementById("contenedorProductos");
 
+function crearTarjetaProducto({id, title, price, image}) {
+
+    return `
+        <div class="col-md-6 col-lg-4 col-xl-3">
+            <div class="card h-100 shadow-sm">
+
+                <img
+                    src="${image}"
+                    class="card-img-top p-3 imagenProducto"
+                    alt="${title}">
+
+                <div class="card-body d-flex flex-column">
+
+                    <h5 class="card-title">
+                        ${title}
+                    </h5>
+
+                    <p class="fw-bold text-success">
+                        $${price}
+                    </p>
+
+                <button
+                    class="btn btn-primary mt-auto botonVerDetalles"
+                    data-id="${id}">
+                    Ver detalles
+                </button>
+
+                </div>
+
+            </div>
+        </div>
+    `;
+
+}
+
 export function mostrarProductos(listaProductos) {
 
     let tarjetasProductos = "";
 
     listaProductos.forEach((producto) => {
-        const tarjetaProducto = `
-            <div class="col-md-6 col-lg-4 col-xl-3">
-                <div class="card h-100 shadow-sm">
-                    <img
-                        src="${producto.image}"
-                        class="card-img-top p-3"
-                        alt="${producto.title}"
-                        style="height:250px; object-fit:contain;">
-                    <div class="card-body d-flex flex-column">
-                        <h5 class="card-title">
-                            ${producto.title}
-                        </h5>
-                        <p class="fw-bold text-success">
-                            $${producto.price}
-                        </p>
-                        <button class="btn btn-primary mt-auto">
-                            Ver detalles
-                        </button>
-                    </div>
-                </div>
-            </div>    
-        `;
-        
-        tarjetasProductos += tarjetaProducto;
+
+        tarjetasProductos += crearTarjetaProducto(producto);
 
     });
 
     contenedorProductos.innerHTML = tarjetasProductos;
+
 }
